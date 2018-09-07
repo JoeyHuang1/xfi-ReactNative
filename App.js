0fa3b5c5a3e35d3abb41228bbc67057562ef6cfe
Joey Huang <JoeyHuangOne@gmai.com>
2018-09-06 18:12:31 -0700

    add animation in app.js

--- App.js
+++ App.js
@@ -7,10 +7,15 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, LayoutAnimation, NativeModules} from 'react-native';
 import Login from './src/Login.js'
 import ThermoList from './src/ThermoList.js'
 import { connect } from 'react-redux'
+
+const { UIManager } = NativeModules;
+
+UIManager.setLayoutAnimationEnabledExperimental &&
+  UIManager.setLayoutAnimationEnabledExperimental(true);
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -31,7 +36,34 @@ class App extends Component<Props> {
   }
 }
 
+// Spring 
+var CustomLayoutSpring = {
+  duration: 900,
+  create: {
+    type: LayoutAnimation.Types.spring,
+    property: LayoutAnimation.Properties.scaleXY,
+    springDamping: 0.7,
+  },
+  update: {
+    type: LayoutAnimation.Types.spring,
+    springDamping: 0.7,
+  },
+};
+
+// Linear with easing
+var CustomLayoutLinear = {
+  duration: 600,
+  create: {
+    type: LayoutAnimation.Types.linear,
+    property: LayoutAnimation.Properties.opacity,
+  },
+  update: {
+    type: LayoutAnimation.Types.curveEaseInEaseOut,
+  },
+};
+
 const mapStateToProps = function(state) {
+  LayoutAnimation.configureNext(CustomLayoutSpring);
   return {...state.loginReducer};
 }
 
